4a29b999bcf2a8f92df279f2b19397f3fa88709b
znathanqueiroz-a11y <z.nathan.queiroz@gmail.com>
2026-09-04 17:45:34 -0300

    Debug sessao render

--- restaurar_sessao.js
+++ restaurar_sessao.js
@@ -5,6 +5,7 @@ const secret = "/etc/secrets/auth_session.tar.gz";
 
 if (fs.existsSync(secret) && !fs.existsSync("auth_info_baileys")) {
   fs.copyFileSync(secret, "auth_session.tar.gz");
+  console.log(fs.readFileSync(secret).subarray(0,16));
   execSync("tar -xzf auth_session.tar.gz");
   console.log("✅ Sessão WhatsApp restaurada");
 }
